f70284681fbe7ef8d198e14b7acfbb7b31f0cde4
davidsung <dks99455@gmail.com>
2022-06-18 12:46:16 -0700

    additional file structure refactors to account for react 18 reqs

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+// import NavBar from './components/NavBar.jsx';
+// import FrontPage from './components/FrontPage.jsx';
+// import About from './components/About.jsx';
+// import SoftwareDev from './components/SoftwareDev.jsx';
+// import Hobbies from './components/Hobbies.jsx';
 
 export default function App() {
   return <h1>Hello World</h1>;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
