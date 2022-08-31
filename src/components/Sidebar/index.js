7f098540608801671a470f81a9e0b31293efd1c0
davidsung <dks99455@gmail.com>
2022-08-30 17:08:13 -0700

    minor fixes to font and links css

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,7 +4,7 @@ import LogoS from '../../assets/images/logo-s.png';
 import LogoSubtitle from '../../assets/images/logo_sub.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { faLinkedin, faGithub, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => (
   <div className='nav-bar'>
@@ -32,11 +32,6 @@ const Sidebar = () => (
         <li>
           <a target='_blank' rel='noreferrer' href='https://github.com/dks99455'>
             <FontAwesomeIcon icon={faGithub} color='#4d4d4e'/>
-          </a>
-        </li>
-        <li>
-          <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/davidksung/'>
-            <FontAwesomeIcon icon={faYoutube} color='#4d4d4e'/>
           </a>
         </li>
       </ul>
