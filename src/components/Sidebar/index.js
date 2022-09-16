855f254b088962b56cb34575e5bfe2eabddb8c7c
davidsung <dks99455@gmail.com>
2022-09-15 22:22:28 -0700

    layout adjustments

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,5 @@
 import { Link, NavLink } from 'react-router-dom';
 import './index.scss';
-import LogoS from '../../assets/images/logo-s.png';
-import LogoSubtitle from '../../assets/images/logo_sub.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
@@ -9,8 +7,8 @@ import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 const Sidebar = () => (
   <div className='nav-bar'>
       <Link className='logo' to='/'>
-        <img src={LogoS} alt='logo' />
-        <img className='sub-logo' src={LogoSubtitle} alt='logosubtitle' />
+        HOME LOGO{/* <img src={} alt='logo' />
+        <img className='sub-logo' src={LogoSubtitle} alt='logosubtitle' /> */}
       </Link>
       <nav>
         <NavLink exact='true' activeclassname='active' to='/'>
