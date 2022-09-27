9dddae7041f4a0c33de2106034a6423baa07d27d
davidsung <dks99455@gmail.com>
2022-09-26 18:19:35 -0700

    mobile-friendly layout partial completion

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -32,10 +32,10 @@ const About = () => {
             idx={15}
           />
         </h1>
-        <p>It took a while to find my passion after college, but I found it in quality assurance at MnSoft. With no background in computer science, I made some friends and picked up the know-how needed to replicate bugs and submit feature requests. I was regularly exposed to the software development process and wanted to delve deeper into it.</p>
+        <p>I found my initial passion for tech in quality assurance at MnSoft. With no background in computer science, I made some friends and picked up the know-how needed to replicate bugs and submit feature requests. I was regularly exposed to the software development process and wanted to delve deeper into it.</p>
         <p>Fast forward to 2022, and I enrolled into Hack Reactor, a 13-week software engineering bootcamp consisting of over 1000 hours of full stack development in an AGILE environment.</p>
         <p>In a little over 5 months I went from taking introductory CompSci courses to a full understanding of the software development lifecycle.</p>
-        <p>Check out my portfolio to see what I can do.</p>
+        <p>Please see my portfolio to check out what I've done.</p>
       </div>
 
       <div className='stage-cube-cont'>
