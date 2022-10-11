5bb152a621ac026077714aaad4021ad3eefe0ec6
davidsung <dks99455@gmail.com>
2022-10-10 19:09:21 -0700

    links revised

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -17,7 +17,7 @@ const Sidebar = () => (
         <NavLink exact='true' activeclassname='active' className='about-link' to='/about'>
           <FontAwesomeIcon icon={faUser} color='#4d4d4e' />
         </NavLink>
-        <NavLink exact='true' activeclassname='active' className='portfolio-link' to='/portfolio'>
+        <NavLink exact='true' activeclassname='active' className='portfolio-link' to='/projects'>
           <FontAwesomeIcon icon={faFolderOpen} color='#4d4d4e' />
         </NavLink>
         <NavLink exact='true' activeclassname='active' className='contact-link' to='/contact'>
